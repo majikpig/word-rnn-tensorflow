bd5f0ef74cc04d2849a2a83bddf5f30d1ce7d572
majikpig <majikpig@gmail.com>
2018-11-22 18:13:55 -0500

    Update sample.py
    Changed defaults to Beam search, 1500 words, and sample on spaces

--- sample.py
+++ sample.py
@@ -14,15 +14,15 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save',
                        help='model directory to load stored checkpointed models from')
-    parser.add_argument('-n', type=int, default=200,
+    parser.add_argument('-n', type=int, default=1500,
                        help='number of words to sample')
     parser.add_argument('--prime', type=str, default=' ',
                        help='prime text')
-    parser.add_argument('--pick', type=int, default=1,
+    parser.add_argument('--pick', type=int, default=2,
                        help='1 = weighted pick, 2 = beam search pick')
     parser.add_argument('--width', type=int, default=4,
                        help='width of the beam search')
-    parser.add_argument('--sample', type=int, default=1,
+    parser.add_argument('--sample', type=int, default=2,
                        help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
     parser.add_argument('--count', '-c', type=int, default=1,
                        help='number of samples to print')
